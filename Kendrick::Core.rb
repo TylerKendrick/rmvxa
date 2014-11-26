fab23095675286aa83765232ffbc3c7d9189cbfd
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2014-11-26 17:37:34 -0600

    Update Kendrick::Core.rb
    Removed Kendrick::Core::Noted.
    Added basic structure for error handling.
    Added basic structure for parsing escape characters
    Added common operations to Game_Battler
    Added #auniq method to ::Array
    Moved Kendrick::Core::Script to Kendrick::Script
    Renamed variables to adhere to common naming convention

--- Kendrick::Core.rb
+++ Kendrick::Core.rb
@@ -2,7 +2,7 @@
 #===============================================================================
 Author:     Tyler Kendrick
 Title:      Kendrick - Core
-Version:    v1.1.1
+Version:    v2.0.0
 
 Language:   RGSS3
 Framework:  RPG Maker VX Ace
@@ -37,49 +37,11 @@ Description:
 The following core script enables common operations for use with other Kendrick
 scripts.
 
-This script allows for parsing game data note sections for well-formatted,
-registered tags.  To register tags, simply append to the Tags variable:
-i.e. Kendrick::Core::Noted.Tags += [ :new_value ]
-
-In the note sections, the tags must be enclosed - either with a matching named
-end-tag ("<name>content<name/>") or through a self enclosed tag ("<name/>").
-
-Tags may also include attributes as named key/value pairs. Valid attribute keys
-must be at least once space away from the tag's name and must not contain any
-punctuation or special characters.  Valid attribute values must be contained
-within quotation marks and may not contain escape characters.
-
-  Valid Examples:
-    <name attr="value"/>
-    <name atr="3"></name>
-    <name value="'attr'" />
-  Invalid Examples:
-    <nameattr="value"/>
-    <name atr=3></name>
-    <name value="\'attr\'" />
-
 --------------------------------------------------------------------------------
 Setup:
 
-1.) Place this script in the materials section, above all other scripts that require
+Place this script in the materials section, above all other scripts that require
 Kendrick - Core.
-
-2.) Add tags that you wish to parse to Kendrick::Core:Noted.Tags
-i.e. Kendrick::Core::Noted.Tags += [ :my_tag ]
-
-3.) Create a new class that implements Kendrick::Core::Noted as a mixin
-
-i.e. 
-    class MY_CLASS
-      include Kendrick::Core::Noted
-      
-      def parse_tag(tag)
-        case tag.name
-        when :my_tag
-          msgbox_p("TAG FOUND") 
-        end
-      end  
-    end # MY_CLASS
 
 #===============================================================================
 =end
@@ -90,225 +52,182 @@ $imported["Kendrick::Core"] = true
 # namespace prefix.
 #===============================================================================
 module Kendrick
+  #=============================================================================
+  # Note: This object contains the majority of data structures in use by
+  # derived Kendrick Scripts.
+  #=============================================================================  
   module Core
+    #===========================================================================
+    # Note: This Hash exists to provide common errors.
+    #===========================================================================
+    @@errors = {
+      # Use: Errors[:missing_method].call(self, __method__)
+      :missing_method => proc { |context, method|
+        className = context.class.name
+        message = "#{method} must be implemented on #{className}"
+        ::NotImplementedError.new(message)
+      }
+    } # Kendrick::Core::Errors
+    def self.Errors(values)
+      return @@errors = @@errors.merge(values)
+    end
+    #===========================================================================
+    # Note: This construct only exists to store control characters for windows.
+    #===========================================================================
+    @@escape_characters = {
+      :tab => '\t',
+      :new_line => '\n',
+      :carriage_return => '\r',
+      :form_feed => '\f',
+      :backspace => '\b',
+      :bell => '\a',
+      :escape => '\e',
+      :space => '\s',
+    }
+    def self.Escape_Characters(values = nil)
+      @@escape_characters = @@escape_characters.merge(values) if values
+      return @@escape_characters
+    end
     #===========================================================================
     # Note: This construct only exists to store regular expressions for 
     # consumption in Kendrick Scripts.
     #===========================================================================
     Regex = {
-      :XmlTag => /[<|>|\&]/,
-      :XmlCode => /(\&\#(\w|\d)+;)/,
-      :Quotes => /"([^"]*)"/,
-      :Tag => /^<(\w+)([^<]+)*(?:>(.*)<\/(?:\s)*\1>|(\s*)+\/>)$/m,
-      :Attribute => /(\w+)\=("([^"]*)"){1}/
+      :xml_tag => /[<|>|\&]/,
+      :xml_code => /(\&\#(\w|\d)+;)/,
+      :quotes => /"([^"]*)"/,
+      :xml_tag => /^<(\w+)([^<]+)*(?:>(.*)<\/(?:\s)*\1>|(\s*)+\/>)$/m,
+      :xml_attribute => /(\w+)\=("([^"]*)"){1}/
     } # Kendrick::Core::Regex
     #===========================================================================
-    # Note: This constant exists only because I was unable to get other methods
-    # of HTML text encoding/decoding to work correctly.
-    # 
-    # Below are the following lines and errors:
-    # CGI.escapeHTML("#{m}")        # Error: CGI Module doesn't exist
-    # "#{m}".encode(:xml => :text)  # Error: converter not found (xml_text)
+    # Note: This method enables custom initializatiion after the database has 
+    # loaded.
     #===========================================================================
-    XmlCodes = {
-      '<' => "&#lt;",
-      '>' => "&#gt;",
-      '&' => "&amp;",
-      '"' => "&quot;"
-    } # Kendrick::Core::XmlCodes
-    #===========================================================================
-    # Note: This module is meant to act as a common mixin for addons.
-    #===========================================================================
-    module Script
-      @@dependencies = []
-      
-      class << self
-        def Dependencies(values = nil)
-          @@dependencies = (@@dependencies + values).uniq if !values.nil?
-          return @@dependencies
-        end
-        
-        def resolve_dependencies
-          remaining = @@dependencies - $imported.keys.select { |x| $imported[x] }
-          if !(remaining.nil? || remaining.empty?)
-            names = remaining.join(", ")
-            message = "The following scripts were required, but not found: { #{names} }"
-            raise ::StandardError.new(message)
-          end
-        end
-      end
-    end # Kendrick::Core::Script
-    #===========================================================================
-    # Note: This module exists as a helper object to clean text for parsing with
-    # my Regex::Tags regular expression.  Normally, putting '<' and '>' symbols
-    # in an tag's attribute value would break the regular expression.  However,
-    # this construct allows for symbols to be encoded/decoded into safe symbols
-    # that can be parsed by my regular expression.
-    #===========================================================================
-    class ::String
-      def self.xml_encode(text) # Replaces text in quotes with HTML codes.
-        return text.gsub(Regex[:Quotes]) { |m|
-          # Encodes xml tags braces to their named code.
-          "#{m}".gsub(Regex[:XmlTag]) { |x| XmlCodes["#{x}"] }
-        }
-      end
-      
-      def self.xml_decode(text) # Replaces HTML codes in quotes with text. 
-        return text.gsub(Regex[:Quotes]) { |m| 
-          # Decodes xml codes to their symbol.
-          "#{m}".gsub(Regex[:XmlCode]) { |x| XmlCodes.index("#{x}") }
-        }
-      end
-        
-      def xml_encode()
-        return ::String.xml_encode(self)
-      end
-      
-      def xml_decode()
-        return ::String.xml_decode(self)
-      end
-    end # ::String
-    #===========================================================================
-    # Note: This module reads note sections on objects with a "note" field.  By
-    # using "include Kendrick::Noted" in a class or module, specified notetags
-    # can be parsed and handled from the "note" field.
-    #===========================================================================
-    module Noted
-      @@tags = [] # Append symbols through mixins to filter valid tags.
-
-      attr_reader :tags # The tags found on the instance object.
-
-      def self.Tags(values = nil)
-        @@tags = (@@tags + values).uniq if !values.nil?
-        @@tags.collect { |x| x.to_sym } # Ensure all are read as symbols
-      end
-      
-      def setup # Provides a setup method for object constructed without initialize.
-        @tags = Tag.parse(self.note.xml_encode)
-        # parse_tag MUST be implemented for mixins
-        @tags.each_value { |tag| parse_tag(tag) }
-      end
-      
-      class Tag
-        attr_reader :name
-        attr_reader :innerText
-        
-        Attribute = Struct.new(:name, :value) do
-          def self.valid?(text)
-            return Regex[:Attribute].match(text)
-          end
-          
-          def self.parse(text)
-            return text.xml_decode.split.collect { |attr| 
-              raise MalFormattedNotetagError.new unless valid?(attr)
-              pair = attr.split('=')
-              Attribute.new(pair[0], pair[1].delete('"'))
-            }
-          end
-          
-          def to_s
-            return sprintf('%s="%s"', name, value)
-          end
-          
-          def inspect
-            return to_s
-          end
-        end # Kendrick::Core::Noted::Tag::Atrribute
-                
-        def initialize(name, innerText, attributes)
-          @name = name
-          @innerText = innerText
-          @attributes = attributes if attributes.is_a?(::Hash)
-          @attributes = Array.to_h(attributes) { |x| x.name } if attributes.is_a?(::Array)
-          if @attributes.nil?
-            raise TypeError.new("Expected type of Hash or Array.") 
-          end
-          if !valid_attributes?(@attributes)
-            raise StandardError.new("Attributes collection was constructed improperly") 
-          end
-        end
-                
-        def [](name)
-          return @attributes[name]
-        end
-        
-        def value
-          return @attributes["value"] ? @attributes["value"].value : @innerText
-        end
-        
-        def self.parse(text)
-          tags = {}
-          Regex[:Tag].match(text) { |m|
-            # Must convert to symbol to match Tags
-            name = m[1].intern            
-            # This allows unspecified tags to skip format checks and parsing.
-            next unless Noted.Tags.include?(name)
-            # Convert to has by name for tag indexing.
-            attributes = Attribute.parse(m[2])
-            innerText = m[3].xml_decode if !m[3].empty?
-            tag = Tag.new(name, innerText, attributes)
-            tags[name] = tag
-          }
-          return tags
-        end
-
-        def to_s
-          attrs = @attributes.values
-            .collect{ |a| " " + a.to_s }
-            .reduce(:+)
-          if @innerText.nil? || @innerText.empty?
-            return "<#@name#{attrs}/>"
-          else
-            return "<#@name#{attrs}>#@innerText</#@name>"
-          end  
-        end
-        
-        private
-        def valid_attributes?(attributes)
-          return attributes.is_a?(::Hash) && attributes.all? { |pair|
-            attributes.keys.all? { |key| key.is_a?(::String) } &&
-            attributes.values.all? { |value| value.is_a?(Attribute) }
-          }
-        end
-      end # Kendrick::Core::Noted::Tag
-      #=========================================================================
-      # Note: This class signifies and error in the way a user-specified tag was 
-      # constructed inside of the note section.
-      #=========================================================================
-      class MalFormattedNotetagError < Exception      
-        def initialize()
-          super("The note section contained a mal-formatted tag.")
-        end
-      end # Kendrick::Core::Noted::MalFormattedNotetagError
-    end # Kendrick::Core::Noted
-    
     def self.load_database
-      Kendrick::Core::Script.resolve_dependencies
+      Kendrick::Script.resolve_dependencies
     end
   end # Kendrick::Core
+  #=============================================================================
+  # Note: This module is meant to act as a common mixin for addons.
+  #=============================================================================
+  module Script
+    @@dependencies = []      
+    def self.Dependencies(values = nil)
+      return @@dependencies = @@dependencies.auniq(values)
+    end
+    
+    def self.resolve_dependencies
+      remaining = @@dependencies - $imported.keys.select { |x| $imported[x] }
+      if !(remaining.nil? || remaining.empty?)
+        names = remaining.join(", ")
+        message = "The following scripts were required, but not found: { #{names} }"
+        raise ::StandardError.new(message)
+      end
+    end
+  end # Kendrick::Script
 end # Kendrick
 #===============================================================================
-# DataManager
+# ::Game_Battler
 #===============================================================================
-module DataManager
+class ::Game_Battler < ::Game_BattlerBase
+  def skill? 
+    return current_action && current_action.item.is_a?(::RPG::Skill)
+  end
+  
+  def skill
+    result = skill? ? current_action.item : last_skill
+    return $data_skills[result.id]
+  end
+  
+  def data
+    return actor if actor?
+    return enemy if enemy?
+  end
+end # RPG::Game_Battler
+#===============================================================================
+# ::DataManager
+#===============================================================================
+module ::DataManager
   class << self
     alias :kendrick_core_load_database :load_database
+    def load_database
+      kendrick_core_load_database
+      Kendrick::Core.load_database
+    end
   end
-  def self.load_database
-    kendrick_core_load_database
-    Kendrick::Core.load_database
-  end
-end # DataManager
+end # ::DataManager
 #===============================================================================
-# Array
+# Note: This override allows for custom control characters to be parsed.
+#===============================================================================
+class ::Window_Base < ::Window
+  alias :kendrick_data_levels_convert_escape_characters :convert_escape_characters
+  def convert_escape_characters(text)
+    text = kendrick_data_levels_convert_escape_characters(text)
+    Kendrick::Core.Escape_Characters.each_pair { |key, value|
+      if text.include?(value)
+        converted = convert_escape_character(key)
+        text = text.gsub(/#{value}/, converted)
+      end
+    }
+    return text
+  end
+  
+  def convert_escape_character(key)
+    return Kendrick::Core::Escape_Characters[key]
+  end
+end # ::Window_Base
+#===============================================================================
+# ::Array
 #===============================================================================
 class ::Array
   # Uses a block to select the key for the hash
   def self.to_h(array, &key)
-    return Hash[array.collect { |item| [key.call(item), item] }]
+    return ::Hash[array.collect { |item| [key.call(item), item] }]
+  end
+  
+  # Adds two arrays together and filters for unique results.
+  def self.auniq(array, other, &selector)
+    result = other ? (array + other).uniq : array
+    return result
   end
   
   def to_h(&key)
-    return ::Array.to_h(self)
+    return ::Array.to_h(self) { |item| key.call(item) }
+  end
+
+  def auniq(other, &selector)
+    return ::Array.auniq(self, other) { |item| selector.call(item) }
   end
 end # ::Array
+#===============================================================================
+# Note: This module exists as a helper object to clean text for parsing with my 
+# Regex::Tags regular expression.  Normally, putting '<' and '>' symbols in a 
+# tag's attribute value would break the regular expression.  However, this 
+# construct allows for symbols to be encoded/decoded into safe symbols that can 
+# be parsed by my regular expression.
+#===============================================================================
+class ::String
+  include Kendrick
+  
+  def self.xml_encode(text) # Replaces text in quotes with HTML codes.
+    return text.gsub(Core::Regex[:quotes]) { |m|
+      # Encodes xml tags braces to their named code.
+      "#{m}".gsub(Core::Regex[:xml_tag]) { |x| Noted::XmlCodes["#{x}"] }
+    }
+  end
+  
+  def self.xml_decode(text) # Replaces HTML codes in quotes with text. 
+    return text.gsub(Core::Regex[:quotes]) { |m| 
+      # Decodes xml codes to their symbol.
+      "#{m}".gsub(Core::Regex[:xml_code]) { |x| Noted::XmlCodes.index("#{x}") }
+    }
+  end
+    
+  def xml_encode()
+    return ::String.xml_encode(self)
+  end
+  
+  def xml_decode()
+    return ::String.xml_decode(self)
+  end
+end # ::String
