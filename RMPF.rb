5f9beedd50d6626f1d42c2bb4440b812cec51190
Tyler Kendrick <contact.tylerkendrick@gmail.com>
2014-12-22 21:49:26 -0600

    Create RMPF.rb
    v0.0.1 RMPF
    - initial commit

---
+++ RMPF.rb
@@ -0,0 +1,19 @@
+=begin
+#===============================================================================
+Author:     Tyler Kendrick
+Title:      Rpg Maker Presentation Foundation
+Version:    v0.0.1
+
+Language:   RGSS3
+Framework:  RPG Maker VX Ace
+Git:        https://github.com/TylerKendrick/rmvxa
+--------------------------------------------------------------------------------
+=end
+$imported ||= {}
+$imported["Kendrick::RMPF"] = "v0.0.1"
+
+module Kendrick::RMPF
+  class Binding;    end
+  class Style;      end
+  class Control;    end
+end
